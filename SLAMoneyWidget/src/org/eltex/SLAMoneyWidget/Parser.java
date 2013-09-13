0f8c7a1c52ddbe00d32063f7d43b7816bef1369b
user7 <user7@localhost>
2013-09-13 19:25:15 +0700

    if you will kiss my as, i will touch your dik=***

--- SLAMoneyWidget/src/org/eltex/SLAMoneyWidget/Parser.java
+++ SLAMoneyWidget/src/org/eltex/SLAMoneyWidget/Parser.java
@@ -1,8 +1,19 @@
 package org.eltex.SLAMoneyWidget;
+
+import java.io.File;
+
+import org.xmlpull.v1.XmlPullParser;
+
 public class Parser {
-	
-	public Parser(String dirName, String fileName){
-		
+
+	public Parser(File file) {
+
+		try {
+			XmlPullParser xpp = (XmlPullParser) file;
+		} catch (Exception e) {
+
+		}
+
 	}
-	
+
 }
