b484d655b226308dfe06fd68ba95ac907e8d5dce
userValeriya <valeriya.u.sh@mail.ru>
2013-09-20 18:12:56 +0700

    58585

--- SLAMoneyWidget/src/org/eltex/SLAMoneyWidget/MyWidget.java
+++ SLAMoneyWidget/src/org/eltex/SLAMoneyWidget/MyWidget.java
@@ -2,16 +2,19 @@ package org.eltex.SLAMoneyWidget;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import android.R;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.util.Log;
+import android.widget.TextView;
 
 
 public class MyWidget extends AppWidgetProvider {
 	final String LOG_TAG = "myLogs";
 	
 	ArrayList<String> data;
+	TextView tvName,tvMoney;
 
 	  @Override
 	  public void onEnabled(Context context) {
@@ -24,8 +27,8 @@ public class MyWidget extends AppWidgetProvider {
 	      int[] appWidgetIds) {
 	    super.onUpdate(context, appWidgetManager, appWidgetIds);
 	    Log.d(LOG_TAG, "onUpdate " + Arrays.toString(appWidgetIds));
-	    AndreyzUpDater aud= new AndreyzUpDater("http://www.cbr.ru/scripts/XML_daily.asp");
-	    data = aud.getSlavaDate();	    
+	   // AndreyzUpDater aud= new AndreyzUpDater("http://www.cbr.ru/scripts/XML_daily.asp");
+	   // data = aud.getSlavaDate();	    
 	  }
 
 	  @Override
