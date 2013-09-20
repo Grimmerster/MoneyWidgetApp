0f01851060b5c3a780aeaaced3bce889c5155233
user7 <user7@localhost>
2013-09-20 17:30:39 +0700

    Class complete

--- SLAMoneyWidget/src/org/eltex/SLAMoneyWidget/Parser.java
+++ SLAMoneyWidget/src/org/eltex/SLAMoneyWidget/Parser.java
@@ -1,57 +1,99 @@
 package org.eltex.SLAMoneyWidget;
 
-import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
-import android.text.TextUtils;
+import android.util.Log;
 
 public class Parser {
 
-	String tmp = "";
-	
-	public Parser(File file) {
+	final String LOG_TAG = "myLogs";
+	ArrayList<String> returnString;
+	XmlPullParser xpp;
+	boolean flag = false;
+	boolean flag2 = false;
+	boolean flag3 = false;
 
-		try {
-			XmlPullParser xpp = (XmlPullParser) file;
+	public Parser(XmlPullParser file) {
+		xpp = file;
+	}
 
-			while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
-				switch (xpp.getEventType()) {
-				// начало документа
-				case XmlPullParser.START_DOCUMENT:
-					break;
-				// начало тэга
-				case XmlPullParser.START_TAG:
-					tmp = "";
-					for (int i = 0; i < xpp.getAttributeCount(); i++) {
-						tmp = tmp + xpp.getAttributeName(i) + " = "
-								+ xpp.getAttributeValue(i) + ", ";
-					}
-					if (!TextUtils.isEmpty(tmp))
-					break;
-				// конец тэга
-				case XmlPullParser.END_TAG:
-					break;
-				// содержимое тэга
-				case XmlPullParser.TEXT:
-					break;
+	public ArrayList<String> getValues() throws XmlPullParserException,
+			IOException {
 
-				default:
-					break;
+		while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
+			switch (xpp.getEventType()) {
+			// начало документа
+			case XmlPullParser.START_DOCUMENT:
+				break;
+			// начало тэга
+			case XmlPullParser.START_TAG:
+				break;
+			// конец тэга
+			case XmlPullParser.END_TAG:
+				break;
+			// содержимое тэга
+			case XmlPullParser.TEXT:
+				if (flag3) {
+					returnString.add(xpp.getText() + "");
+					flag3 = false;
 				}
-				// следующий элемент
-				xpp.next();
-			}
-		} catch (Exception e) {
+				if (flag2) {
+					flag3 = true;
+					flag2 = false;
+				}
+				try {
+					int nameCode = Integer.parseInt(xpp.getText());
 
+					if (flag) {
+						flag2 = true;
+						flag = false;
+
+					} else {
+						switch (nameCode) {
+						case 826: {
+							Log.d(LOG_TAG,
+									"Фунт стерлингов Соединённого королевства");
+							flag = true;
+							break;
+						}
+						case 840: {
+							Log.d(LOG_TAG, "Доллар США");
+							flag = true;
+							break;
+						}
+						case 978: {
+							Log.d(LOG_TAG, "Евро");
+							flag = true;
+							break;
+						}
+						case 156: {
+							Log.d(LOG_TAG, "Китайский юань");
+							flag = true;
+							break;
+						}
+						case 392: {
+							Log.d(LOG_TAG, "Японский иен");
+							flag = true;
+							break;
+						}
+						}
+					}
+				} catch (Exception e) {
+				}
+				break;
+
+			default:
+				break;
+			}
+			// следующий элемент
+			xpp.next();
 		}
 
-	}
-	
-	public ArrayList<String> h2m14x6() {
-		
-		return null;
+		return returnString;
 	}
 
 }
